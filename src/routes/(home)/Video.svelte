557a4232ea31920107a093ee522a7139bb6ce0b7
bmehder <bmehder@gmail.com>
2025-07-21 19:46:27 +0100

    resized homepage video width

--- src/routes/(home)/Video.svelte
+++ src/routes/(home)/Video.svelte
@@ -5,5 +5,14 @@
 <section class="space flow center">
   <h2 class="balance">Watch our LIVE recording on: The Signs of Grooming—What Everyone Needs to Know</h2>
   <div class="tagline">A 25-Minute Webinar to Help You Recognize Grooming Before It Starts</div>
-  <Sveltetube id="Qpa88El0r0E" shadow="2px 4px 10px rgba(0,0,0,0.2)" />
+  <div class="video-container">
+    <Sveltetube id="Qpa88El0r0E" shadow="2px 4px 10px rgba(0,0,0,0.2)" />
+  </div>
 </section>
+
+<style>
+  .video-container {
+    max-width: 32rem;
+    margin-inline: auto;
+  }
+</style>
